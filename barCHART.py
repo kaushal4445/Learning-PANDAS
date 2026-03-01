4da23e472fbedd4f615d164cf66afffaab49578d
Kaushal <mehrakaushal54@gmail.com>
2026-03-01 23:52:48 +0530

    Create barCHART.py

---
+++ barCHART.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+Subjects = ["DBMS", "DSA", "OS"]
+Marks = [60, 70, 80]
+
+plt.bar(Subjects, Marks)
+plt.xlabel("Subjects")
+plt.ylabel("Marks")
+plt.title(" BAR CHART")
+plt.show()
