b15fe5620ad7e31c4634fc6a5a2919093d0a5549
Kaushal <mehrakaushal54@gmail.com>
2026-03-02 00:05:14 +0530

    Delete barCHART.py

--- barCHART.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-Subjects = ["DBMS", "DSA", "OS"]
-Marks = [60, 70, 80]
-
-plt.bar(Subjects, Marks)
-plt.xlabel("Subjects")
-plt.ylabel("Marks")
-plt.title(" BAR CHART")
-plt.show()
